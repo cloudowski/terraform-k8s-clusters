e6a5318e0cf9489585f51bb4a24a4506a4184f45
Tomasz Cholewa <tomasz@cloudowski.com>
2020-05-15 20:10:29 +0200

    Handle k8s version param in aks

--- providers/aks/main.tf
+++ providers/aks/main.tf
@@ -20,6 +20,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
   dns_prefix          = local.dns_prefix
+  kubernetes_version  = var.k8s_version
 
   linux_profile {
     admin_username = local.admin_username
